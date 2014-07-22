1e83e75327a2fb95f9c714e6d7c6001b49ed6668
superego <cerci.jj@gmail.com>
2014-07-22 12:08:53 +0800

    修复：在oracle数据库中，如果同时使用了distinct和order by来查询数据，则会提示sql语法错误的问题

--- spirit-dao/src/main/java/com/googlecode/easyec/spirit/dao/dialect/impl/OracleJdbcPageDialect.java
+++ spirit-dao/src/main/java/com/googlecode/easyec/spirit/dao/dialect/impl/OracleJdbcPageDialect.java
@@ -4,6 +4,7 @@ import com.googlecode.easyec.spirit.dao.dialect.JdbcPageDialect;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
 
 /**
  * Oracle数据库类型的分页方言实现类。
@@ -24,7 +25,25 @@ public class OracleJdbcPageDialect extends JdbcPageDialect {
         // build paged sql
         StringBuffer sb = new StringBuffer();
         sb.append("select * from (select t_.*, ROWNUM rn from (");
-        sb.append(jdbcSql);
+
+        // 修复如果sql中同时包含distinct和order by
+        // 关键字查询产生报错的问题
+        Matcher m_d = select_Pattern_d.matcher(jdbcSql);
+        Matcher m_o = order_By_Pattern.matcher(jdbcSql);
+        // 如果同时查找两个关键字，则剔除distinct
+        if (m_d.find() && m_o.find()) {
+            StringBuffer buf = new StringBuffer();
+
+            buf.append("select distinct * from (");
+            m_d.appendReplacement(buf, "");
+            buf.append(
+                m_d.group().substring(m_d.start() + 1)
+            );
+            buf.append(") ");
+
+            sb.append(buf);
+        } else sb.append(jdbcSql);
+
         sb.append(") t_ where ROWNUM <= ?) where rn > ?");
 
         logger.debug("Paged SQL for Oracle: [" + sb + "].");
