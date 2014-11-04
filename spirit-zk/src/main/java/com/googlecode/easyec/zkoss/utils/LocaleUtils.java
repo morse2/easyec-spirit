efd60f2a43019414476eb1c16e61295614da62e5
JunJie <cerci.jj@gmail.com>
2014-11-04 21:01:52 +0800

    新增类：LocaleUtils，为操作Locale提供工具类

---
+++ spirit-zk/src/main/java/com/googlecode/easyec/zkoss/utils/LocaleUtils.java
@@ -0,0 +1,34 @@
+package com.googlecode.easyec.zkoss.utils;
+
+import com.googlecode.easyec.spirit.web.utils.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+import java.util.Map;
+
+/**
+ * 本地化工具类
+ *
+ * @author JunJie
+ */
+public class LocaleUtils {
+
+    private LocaleUtils() { /* no op */ }
+
+    /**
+     * 返回得到本地化查询字符串
+     *
+     * @param pageContext 页面上下文对象
+     * @param locale      本地化字符代码
+     * @return 查询字符串
+     */
+    public static String getLocaleQs(PageContext pageContext, String locale) {
+        Map<String, String> map = WebUtils.decodeQueryString(
+            (HttpServletRequest) pageContext.getRequest()
+        );
+
+        map.put("locale", locale);
+
+        return WebUtils.encodeQueryString(map);
+    }
+}
