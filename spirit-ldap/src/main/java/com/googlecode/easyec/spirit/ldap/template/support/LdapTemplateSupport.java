524c79f11aa912c4069704b9721253d0a78c8b09
JunJie <cerci.jj@gmail.com>
2015-01-27 22:32:39 +0800

    为LdapTemplateSupport类，增加统一创建LdapItemOperation的方法

--- spirit-ldap/src/main/java/com/googlecode/easyec/spirit/ldap/template/support/LdapTemplateSupport.java
+++ spirit-ldap/src/main/java/com/googlecode/easyec/spirit/ldap/template/support/LdapTemplateSupport.java
@@ -8,7 +8,13 @@ import javax.annotation.Resource;
 import javax.naming.directory.BasicAttribute;
 import javax.naming.directory.ModificationItem;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import static com.googlecode.easyec.spirit.ldap.template.support.LdapItemOperation.LdapItemOperationType.ADD_ITEM;
+import static org.apache.commons.lang.ArrayUtils.isEmpty;
+import static org.apache.commons.lang.ArrayUtils.isEquals;
+import static org.apache.commons.lang.StringUtils.isBlank;
 
 /**
  * LDAP模板操作的支持类
@@ -48,6 +54,57 @@ public abstract class LdapTemplateSupport {
      */
     protected ModificationItem createModificationItem(LdapItemOperation operation) {
         return _createModificationItem(operation.getId(), operation.getValue(), operation.getType().getValue());
+    }
+
+    /**
+     * 依据给定的参数3和参数4的值，进行比较操作，
+     * 来创建一个<code>LdapItemOperation</code>对象，
+     * 并将此对象放入参数1的集合对象中。
+     *
+     * @param coll   LdapItemOperation对象集合
+     * @param key    属性名称
+     * @param oldVal 旧值（在LDAP系统中已存在）
+     * @param newVal 新值（用户给到）
+     */
+    protected void createLdapItemOperation(Collection<LdapItemOperation> coll, String key, String oldVal, String newVal) {
+        if (isBlank(oldVal) && isBlank(newVal)) return;
+        if (isBlank(oldVal)) coll.add(new LdapItemOperation(key, newVal, ADD_ITEM));
+        else if (isBlank(newVal)) coll.add(new LdapItemOperation(key));
+        else if (!oldVal.equals(newVal)) coll.add(new LdapItemOperation(key, newVal));
+    }
+
+    /**
+     * 依据给定的参数3和参数4的值，进行比较操作，
+     * 来创建一个<code>LdapItemOperation</code>对象，
+     * 并将此对象放入参数1的集合对象中。
+     *
+     * @param coll   LdapItemOperation对象集合
+     * @param key    属性名称
+     * @param oldVal 旧值（在LDAP系统中已存在）
+     * @param newVal 新值（用户给到）
+     */
+    protected void createLdapItemOperation(Collection<LdapItemOperation> coll, String key, byte[] oldVal, byte[] newVal) {
+        if (isEmpty(oldVal) && isEmpty(newVal)) return;
+        if (isEmpty(oldVal)) coll.add(new LdapItemOperation(key, newVal, ADD_ITEM));
+        else if (isEmpty(newVal)) coll.add(new LdapItemOperation(key));
+        else if (!isEquals(oldVal, newVal)) coll.add(new LdapItemOperation(key, newVal));
+    }
+
+    /**
+     * 依据给定的参数3和参数4的值，进行比较操作，
+     * 来创建一个<code>LdapItemOperation</code>对象，
+     * 并将此对象放入参数1的集合对象中。
+     *
+     * @param coll   LdapItemOperation对象集合
+     * @param key    属性名称
+     * @param oldVal 旧值（在LDAP系统中已存在）
+     * @param newVal 新值（用户给到）
+     */
+    protected void createLdapItemOperation(Collection<LdapItemOperation> coll, String key, Object oldVal, Object newVal) {
+        if (oldVal == null && newVal == null) return;
+        if (oldVal == null) coll.add(new LdapItemOperation(key, newVal, ADD_ITEM));
+        else if (newVal == null) coll.add(new LdapItemOperation(key));
+        else if (!oldVal.equals(newVal)) coll.add(new LdapItemOperation(key, newVal));
     }
 
     private ModificationItem _createModificationItem(String id, Object value, int modifyOp) {
