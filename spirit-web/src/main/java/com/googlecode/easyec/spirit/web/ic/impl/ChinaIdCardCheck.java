7879310fa72e00d6a19a1c3665efe6690bccef7d
JunJie <cerci.jj@gmail.com>
2015-03-23 23:36:18 +0800

    删除多余代码

--- spirit-web/src/main/java/com/googlecode/easyec/spirit/web/ic/impl/ChinaIdCardCheck.java
+++ spirit-web/src/main/java/com/googlecode/easyec/spirit/web/ic/impl/ChinaIdCardCheck.java
@@ -16,9 +16,6 @@ import java.util.regex.Pattern;
  */
 abstract class ChinaIdCardCheck implements IdCard {
 
-    private static final String LEAP_YEAR_15 = "^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}$";
-    private static final String AVRG_YEAR_15 = "^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))[0-9]{3}$";
-
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     private String regexForLeapYear;
