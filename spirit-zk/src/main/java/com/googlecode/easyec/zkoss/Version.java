23f664beed6cf8fb9e4369525ab4eb9d07a80d64
JunJie <cerci.jj@gmail.com>
2015-12-17 14:33:34 +0800

    增加ZK版本类, 并且更新tld/config.xml, 以适应ZK 8的自定义标签

---
+++ spirit-zk/src/main/java/com/googlecode/easyec/zkoss/Version.java
@@ -0,0 +1,13 @@
+package com.googlecode.easyec.zkoss;
+
+/**
+ * The version of ZK Spirit library.
+ * {@link #UID} must be the same as the version specified in config.xml
+ *
+ * @author JunJie
+ * @since 0.6.3
+ */
+public class Version {
+
+    public static final String UID = "0.6.3";
+}
