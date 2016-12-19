b45e9df4c7a7571e0647baaa11a08cd0ea9109ec
JunJie <cerci.jj@gmail.com>
2016-12-19 15:50:00 +0800

    修复问题：对于操作MyBatis缓存时候，存取缓存会发生不一致的错误

--- spirit-mybatis/src/main/java/com/googlecode/easyec/spirit/mybatis/cache/CacheServiceCache.java
+++ spirit-mybatis/src/main/java/com/googlecode/easyec/spirit/mybatis/cache/CacheServiceCache.java
@@ -1,6 +1,7 @@
 package com.googlecode.easyec.spirit.mybatis.cache;
 
 import com.googlecode.easyec.caching.CacheService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.cache.Cache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,17 +19,16 @@ import static com.googlecode.easyec.spirit.web.utils.SpringContextUtils.getBean;
  */
 public class CacheServiceCache implements Cache {
 
-    /**
-     * MyBatis缓存使用到的名字
-     */
-    public static final String MYBATIS_CACHE_NAME = "mybatisCache";
-
     private static final Logger logger = LoggerFactory.getLogger(CacheServiceCache.class);
     private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
     private CacheService cacheService;
     private boolean initialized;
 
-    private String id;
+    private boolean useGlobalCache = true;  // 标示是否使用全局的缓存
+    private String cacheServiceName;        // 缓存服务的名称
+    private String id;                      // 当前DAO ID
+
+    public static final String DEFAULT_CACHE_NAME = "mybatisCache";
 
     public CacheServiceCache(String id) {
         Assert.notNull(id, "Mybatis Cache id of prefix cannot be null.");
@@ -48,26 +48,26 @@ public class CacheServiceCache implements Cache {
     public void putObject(Object key, Object value) {
         initialize();
         if (null != value) {
-            boolean b = this.cacheService.put(MYBATIS_CACHE_NAME, key, value);
+            boolean b = this.cacheService.put(getTargetCacheName(), key, value);
             logger.debug("Result of putting into cache. [{}]", b);
         }
     }
 
     public Object getObject(Object key) {
         initialize();
-        return this.cacheService.get(MYBATIS_CACHE_NAME, key);
+        return this.cacheService.get(getTargetCacheName(), key);
     }
 
     public Object removeObject(Object key) {
         Object o = getObject(key);
-        boolean b = this.cacheService.removeValue(MYBATIS_CACHE_NAME, key);
+        boolean b = this.cacheService.removeValue(getTargetCacheName(), key);
         logger.debug("Result of removing from cache. [{}]", b);
         return o;
     }
 
     public void clear() {
         initialize();
-        this.cacheService.removeAllValues(this.id);
+        this.cacheService.removeAllValues(getTargetCacheName());
     }
 
     public ReadWriteLock getReadWriteLock() {
@@ -85,12 +85,36 @@ public class CacheServiceCache implements Cache {
             if (null == getCacheService()) {
                 logger.debug("CacheService object is null, so find from Spring context.");
 
-                setCacheService(getBean(CacheService.class));
+                if (StringUtils.isNotBlank(cacheServiceName)) {
+                    setCacheService(getBean(cacheServiceName, CacheService.class));
+                } else setCacheService(getBean(CacheService.class));
+
                 Assert.notNull(cacheService, "CacheService object is still null. Is there no CacheService instance?");
             }
 
             setInitialized(true);
         }
+    }
+
+    /**
+     * 返回当前缓存服务类的名称。
+     * 该名称用于获取目标的缓存服务对象实例
+     *
+     * @return 缓存服务名称
+     */
+    public String getCacheServiceName() {
+        return cacheServiceName;
+    }
+
+    /**
+     * 设置当前缓存服务类的名称。
+     * 设置该名称之后，该类在初始化的时候，
+     * 会使用此名称查找Spring范围内的缓存服务对象实例
+     *
+     * @param cacheServiceName 缓存服务名称
+     */
+    public void setCacheServiceName(String cacheServiceName) {
+        this.cacheServiceName = cacheServiceName;
     }
 
     /**
@@ -129,6 +153,24 @@ public class CacheServiceCache implements Cache {
         this.initialized = initialized;
     }
 
+    /**
+     * 返回当前缓存对象是否使用全局缓存
+     *
+     * @return boolean值
+     */
+    public boolean isUseGlobalCache() {
+        return useGlobalCache;
+    }
+
+    /**
+     * 设置当前缓存对象是否使用全局缓存
+     *
+     * @param useGlobalCache boolean值
+     */
+    public void setUseGlobalCache(boolean useGlobalCache) {
+        this.useGlobalCache = useGlobalCache;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -139,5 +181,14 @@ public class CacheServiceCache implements Cache {
     @Override
     public int hashCode() {
         return id.hashCode();
+    }
+
+    /**
+     * 返回当前目标缓存名称
+     *
+     * @return 缓存名称
+     */
+    protected String getTargetCacheName() {
+        return isUseGlobalCache() ? DEFAULT_CACHE_NAME : id;
     }
 }
