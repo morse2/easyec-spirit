e093a3bdf039da29d113f27244a11b09329c38ea
JunJie.Z <cerci.jj@gmail.com>
2019-09-10 11:40:48 +0800

    优化MybatisPagingInterceptor拦截的定义，优化为只针对Dao对象进行拦截

--- spirit-mybatis/src/main/java/com/googlecode/easyec/spirit/mybatis/paging/support/MybatisPagingInterceptor.java
+++ spirit-mybatis/src/main/java/com/googlecode/easyec/spirit/mybatis/paging/support/MybatisPagingInterceptor.java
@@ -57,7 +57,7 @@ public class MybatisPagingInterceptor implements PagingInterceptor, Ordered {
         this.order = order;
     }
 
-    @Around("execution(* *..*.*Dao.find(..)) && args(page)")
+    @Around("execution(* *..*.*Dao.*(..)) && args(page)")
     public Object intercept(ProceedingJoinPoint joinPoint, Page page) throws Throwable {
         // Since 0.6.4.1
         if (!(page instanceof MybatisPage)) {
